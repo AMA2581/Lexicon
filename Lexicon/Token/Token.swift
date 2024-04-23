011aed29b04c5891e41ef3ae09672d2644a431d6
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-23 17:36:09 +0330

    add token

---
+++ Lexicon/Token/Token.swift
@@ -0,0 +1,29 @@
+//
+//  Token.swift
+//  Lexicon
+//
+//  Created by Amir Mahdi Abravesh on 4/23/24.
+//
+
+import Foundation
+
+class Token {
+    private var token: String
+    
+    init(token: String) {
+        self.token = token
+    }
+    
+    init() {
+        self.token = ""
+    }
+    
+    /// For setting or overwriting the token
+    func setToken(_ input: String) {
+        self.token = input
+    }
+    
+    func getToken() -> String {
+        return token
+    }
+}
