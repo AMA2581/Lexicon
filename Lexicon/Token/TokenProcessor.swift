d9ec21154cc8874b75eedf96402a24a14212cd76
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-24 13:13:18 +0330

    add tokenizer and token processor

---
+++ Lexicon/Token/TokenProcessor.swift
@@ -0,0 +1,12 @@
+//
+//  TokenProcessor.swift
+//  Lexicon
+//
+//  Created by Amir Mahdi Abravesh on 4/24/24.
+//
+
+import Foundation
+
+class TokenProcessor {
+    
+}
---
+++ Lexicon/Token/Tokenizer.swift
@@ -0,0 +1,12 @@
+//
+//  Tokenizer.swift
+//  Lexicon
+//
+//  Created by Amir Mahdi Abravesh on 4/24/24.
+//
+
+import Foundation
+
+class Tokenizer {
+    
+}
