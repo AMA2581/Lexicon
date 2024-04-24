ec85ae3bdf1f66c8cc566b92a18ebc640fe61248
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-24 13:20:46 +0330

    add token types

--- Lexicon/Token/Token.swift
+++ Lexicon/Token/Token.swift
@@ -9,13 +9,16 @@ import Foundation
 
 struct Token {
     private var token: String
+    let type: TokenType
     
-    init(token: String) {
+    init(token: String, type: TokenType) {
         self.token = token
+        self.type = type
     }
     
     init() {
         self.token = ""
+        self.type = TokenType.text
     }
     
     /// For getting the token
---
+++ Lexicon/Token/TokenType.swift
@@ -0,0 +1,14 @@
+//
+//  TokenType.swift
+//  Lexicon
+//
+//  Created by Amir Mahdi Abravesh on 4/24/24.
+//
+
+import Foundation
+
+enum TokenType {
+    case index
+    case text
+    case author
+}
