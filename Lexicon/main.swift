eed0876cb4e2c5ee5d5eb64fe317bbf3e71f2332
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-28 10:10:08 +0330

    refactoring

--- Lexicon/main.swift
+++ Lexicon/main.swift
@@ -12,8 +12,10 @@ var tokenizer = Tokenizer()
 
 let files = fileReader.fileUrlGetter(datasetFolderUrl: URL(string: "/Users/ama25/Documents/Lexicon/Lexicon/cacm/"))
 let content = fileReader.readFile(fileURL: files[1])
+
 let stopWord = fileReader.readFile(fileURL: files[2])
 tokenizer.setStopWord(string: stopWord)
+
 var tokens = tokenizer.tokenizer(data: content)
 
 //for stopword in tokenizer.tokenProceessor.stopWords {
