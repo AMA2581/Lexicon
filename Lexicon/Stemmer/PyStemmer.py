02b679559ea828445487e5e72358077562f20e3b
Mehdi-Nasrollahi <m.nasrollahi82@gmail.com>
2024-04-29 20:47:56 +0330

    add stemmer()

--- Lexicon/Stemmer/PyStemmer.py
+++ Lexicon/Stemmer/PyStemmer.py
@@ -1,23 +1,5 @@
-from nltk.stem import PorterStemmer , SnowballStemmer , WordNetLemmatizer
+from nltk.stem import PorterStemmer, SnowballStemmer
 
-
-stemmer = PorterStemmer()
-new_stemmer = SnowballStemmer("english")
-lem = WordNetLemmatizer()
-
-verbs = ['caresses','mules', 'died', 'flies', 'denied', 'agreed', 'generously', 'caring']
-
-for word in verbs:
-    print(f"{word} --> {stemmer.stem(word)}")
-print("-----------")
-for word in verbs:
-    print(f"{word} --> {new_stemmer.stem(word)}")
-print('--------')
-for word in verbs:
-    print(f"{word} --> {lem.lemmatize(word)}")
-
-
-# def stemmer():
-
-#     pass
-
+def stemmer(str):
+    sbs = SnowballStemmer("english")
+    return sbs.stem(str)
