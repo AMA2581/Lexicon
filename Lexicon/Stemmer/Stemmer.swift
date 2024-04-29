2c0b213312a5f741db84eface47a27169f069353
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-29 09:36:31 +0330

    add arguments

--- Lexicon/Stemmer/Stemmer.swift
+++ Lexicon/Stemmer/Stemmer.swift
@@ -9,12 +9,18 @@ import Foundation
 import PythonKit
 
 class Stemmer {
-    func runPython() -> PythonObject {
+    
+    func Stemmer(_ input: String) {
+        let out = String(runPython(input))
+        
+    }
+    
+    func runPython(_ input: String) -> PythonObject {
         let sys = Python.import("sys")
         sys.path.append("/Users/ama25/Documents/Lexicon/Lexicon/Stemmer/")
         let file = Python.import("PyStemmer")
         
-        let response = file.stemmer()
+        let response = file.stemmer(input)
 //        print(response)
         return response
     }
