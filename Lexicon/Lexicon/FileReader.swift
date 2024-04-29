1025fec435836da04be0fc51a4d7f7eddb8a36fb
Mehdi-Nasrollahi <m.nasrollahi82@gmail.com>
2024-04-29 15:46:17 +0330

    add nltk

---
+++ Lexicon/Lexicon/FileReader.swift
@@ -0,0 +1,56 @@
+//
+//  FileChecker.swift
+//  Lexicon
+//
+//  Created by Amir Mahdi Abravesh on 4/23/24.
+//
+
+import Foundation
+
+struct FileReader {
+    
+    func fileUrlGetter(datasetFolderUrl DFURL: URL?) -> [URL] {
+        let datasetFolderURL = DFURL
+        var output: [URL] = []
+//        print(datasetFolderURL)
+        do {
+            if let datasetFolderURL {
+                let itemsInDirectory = try FileManager.default.contentsOfDirectory(at: datasetFolderURL, includingPropertiesForKeys: nil)
+                for item in itemsInDirectory {
+//                    let fileName: NSString = NSString(string: item.absoluteString)
+                    output.append(item)
+//                    print(fileName)
+                }
+            }
+        } catch {
+            fatalError("\(error)")
+        }
+        
+        return output
+    }
+
+    func fileNameGetter(datasetFolderPath DFStr: String) {
+        do {
+            let itemsInDirectory = try FileManager.default.contentsOfDirectory(atPath: DFStr)
+            for item in itemsInDirectory {
+                let fileName: NSString = NSString(string: item)
+                print(fileName)
+            }
+        } catch {
+            fatalError("\(error)")
+        }
+    }
+    
+    func readFile(fileURL: URL) -> String {
+        var out: String
+        
+        //reading
+        do {
+            out = try String(contentsOf: fileURL, encoding: .utf8)
+        } catch {
+            fatalError("\(error)")
+        }
+        
+        return out
+    }
+}
--- Lexicon/Stemmer/PyStemmer.py
+++ Lexicon/Stemmer/PyStemmer.py
@@ -1,3 +1,23 @@
+from nltk.stem import PorterStemmer , SnowballStemmer , WordNetLemmatizer
 
-def stemmer(self, input):
-    return input
+
+stemmer = PorterStemmer()
+new_stemmer = SnowballStemmer("english")
+lem = WordNetLemmatizer()
+
+verbs = ['caresses','mules', 'died', 'flies', 'denied', 'agreed', 'generously', 'caring']
+
+for word in verbs:
+    print(f"{word} --> {stemmer.stem(word)}")
+print("-----------")
+for word in verbs:
+    print(f"{word} --> {new_stemmer.stem(word)}")
+print('--------')
+for word in verbs:
+    print(f"{word} --> {lem.lemmatize(word)}")
+
+
+# def stemmer():
+
+#     pass
+
