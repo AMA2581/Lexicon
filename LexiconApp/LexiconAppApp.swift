2580a2f6fed2b8ba650dc01265509ca0f1a22390
AMA2581 <amir.abravesh.ama@gmail.com>
2024-04-30 20:19:33 +0330

    add new target

---
+++ LexiconApp/LexiconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiconAppApp.swift
+//  LexiconApp
+//
+//  Created by Amir Mahdi Abravesh on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LexiconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
