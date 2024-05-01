3f03e180e2f674c0f88292f5cdf0187c8cdee7ee
AMA2581 <amir.abravesh.ama@gmail.com>
2024-05-01 13:04:10 +0330

    refactoring

--- LexiconApp/View/FilePickerView.swift
+++ LexiconApp/View/FilePickerView.swift
@@ -5,33 +5,31 @@
 //  Created by Amir Mahdi Abravesh on 5/1/24.
 //
 
-import SwiftUI
 import AppKit
+import SwiftUI
 
 struct FilePickerView: View {
-  @State private var selectedFileURL: URL?
+    @State private var selectedFileURL: URL?
 
-  func pickDocument() {
-    let panel = NSOpenPanel()
-    panel.allowsMultipleSelection = false
-    panel.allowedFileTypes = ["txt", "", "all"] // Adjust for your file types
+    func pickDocument() {
+        let panel = NSOpenPanel()
+        panel.allowsMultipleSelection = false
+        panel.allowedFileTypes = ["txt", "", "all"] // Adjust for your file types
 
-      panel.beginSheetModal(for: .init()) { responce in
-          //
-      }
-      if panel.isVisible {
-      selectedFileURL = panel.url
+        panel.beginSheetModal(for: .init()) { responce in
+            print(panel.url)
+            selectedFileURL = panel.url
+        }
     }
-  }
 
-  var body: some View {
-    Button("Pick File") {
-      pickDocument()
+    var body: some View {
+        Button("Pick File") {
+            pickDocument()
+        }
+        if let url = selectedFileURL {
+            Text("Selected file: \(url.lastPathComponent)")
+        }
     }
-    if let url = selectedFileURL {
-      Text("Selected file: \(url.lastPathComponent)")
-    }
-  }
 }
 
 #Preview {
