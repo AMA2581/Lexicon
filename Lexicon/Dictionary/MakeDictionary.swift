1399619a85abe74453e6d7a1a833807c37fef033
AMA2581 <amir.abravesh.ama@gmail.com>
2024-05-06 10:20:30 +0330

    add logic of dictionary

--- Lexicon/Dictionary/MakeDictionary.swift
+++ Lexicon/Dictionary/MakeDictionary.swift
@@ -8,5 +8,39 @@
 import Foundation
 
 class MakeDictionary {
+    var tokens: [Token]
     
+    init(tokens: [Token]) {
+        self.tokens = tokens
+    }
+    
+    func freqDictionary() -> [String: Int] {
+        var dic: [String: Int] = [:]
+        var checkedTokens: [String] = []
+        
+        for (index, token) in tokens.enumerated() {
+            // checks if the token is already checked
+            if !checkedTokens.contains(token.getToken()){
+                var i = index
+                var counter = 1
+                
+                // counts how many of that word exists
+                while i < tokens.count {
+                    if i != 0 {
+                        if token.getToken() == tokens[i].getToken() {
+                            counter += 1
+                        }
+                    }
+                    
+                    i += 1
+                }
+                
+                // add the token to checked tokens and add to the dictionary
+                checkedTokens.append(token.getToken())
+                dic[token.getToken()] = counter
+            }
+        }
+        
+        return dic
+    }
 }
