58b1470f957dc2deafb8e68c839f8fc16345449d
Amir Abravesh <amir.abravesh.ama@gmail.com>
2024-06-01 10:46:38 +0330

    config dot product

--- Lexicon/CMath/CMath.swift
+++ Lexicon/CMath/CMath.swift
@@ -10,18 +10,20 @@ import Foundation
 class CMath {
     
     /// before calling this function make sure you do the padding first
-    func dotProduct(_ vectorA: [Double], _ vectorB: [Double]) -> Double? {
+    func dotProduct(mainVector vectorA: [Double], inputVector vectorB: [Double]) -> Double? {
+        
+        // TODO: add multi value vector for inputVector
         
         // Check if vectors are of the same length
-        guard vectorA.count == vectorB.count else {
-            print("Vectors must be of the same length")
+        guard vectorB.count == 1 else {
+            print("input vector should have only 1 object")
             return nil
         }
         
         // Calculate the dot product
         var result: Double = 0
         for i in 0..<vectorA.count {
-            result += vectorA[i] * vectorB[i]
+            result += vectorA[i] * vectorB[1]
         }
         
         return result
